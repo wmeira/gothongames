2629ac3d580458df50a2c38bc79adfe9329cb7dc
William Hitoshi Tsunoda Meira <williammeira@gmail.com>
2020-04-02 10:49:28 -0300

    Skeleton

---
+++ setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+config = {
+    'description': 'Final exercise from "Learn Python 3 the Hard Way" (Zed Shaw)',
+    'author': 'William Hitoshi Tsunoda Meira',
+    'url': '',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['gothonweb'],
+    'scripts': [],
+    'name': 'gothonweb'
+}
+
+setup(**config)
